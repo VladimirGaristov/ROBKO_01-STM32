9c40a518e9d6e91480ec053b3dcd4f53a0463344
Vlado <Vlado@Cartogan>
2019-02-19 07:02:35 +0200

    Cloned repo to desktop machine

--- MCU_Firmware/Debug/startup/subdir.mk
+++ MCU_Firmware/Debug/startup/subdir.mk
@@ -18,7 +18,7 @@ startup/%.o: ../startup/%.S
 	@echo 'Building file: $<'
 	@echo 'Invoking: MCU GCC Compiler'
 	@echo $(PWD)
-	arm-none-eabi-gcc -mcpu=cortex-m4 -mthumb -mfloat-abi=hard -mfpu=fpv4-sp-d16 -DSTM32L4 -DSTM32L476RGTx -DNUCLEO_L476RG -DSTM32 -DDEBUG -DUSE_HAL_DRIVER -DSTM32L476xx -I"/home/cartogan/Ac6/workspace/ROBKO_01/MCU_Firmware/inc" -I"/home/cartogan/Ac6/workspace/ROBKO_01/MCU_Firmware/HAL_Driver/Inc" -I"/home/cartogan/Ac6/workspace/ROBKO_01/MCU_Firmware/HAL_Driver/Inc/Legacy" -I"/home/cartogan/Ac6/workspace/ROBKO_01/MCU_Firmware/Utilities/STM32L4xx_Nucleo" -I"/home/cartogan/Ac6/workspace/ROBKO_01/MCU_Firmware/CMSIS/core" -I"/home/cartogan/Ac6/workspace/ROBKO_01/MCU_Firmware/CMSIS/device" -O0 -g3 -Wall -fmessage-length=0 -ffunction-sections -c -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -o "$@" "$<"
+	arm-none-eabi-gcc -mcpu=cortex-m4 -mthumb -mfloat-abi=hard -mfpu=fpv4-sp-d16 -DSTM32L4 -DSTM32L476RGTx -DNUCLEO_L476RG -DSTM32 -DDEBUG -DUSE_HAL_DRIVER -DSTM32L476xx -I"C:/Users/Vlado/workspace/ROBKO_01-STM32/MCU_Firmware/inc" -I"C:/Users/Vlado/workspace/ROBKO_01-STM32/MCU_Firmware/HAL_Driver/Inc" -I"C:/Users/Vlado/workspace/ROBKO_01-STM32/MCU_Firmware/HAL_Driver/Inc/Legacy" -I"C:/Users/Vlado/workspace/ROBKO_01-STM32/MCU_Firmware/Utilities/STM32L4xx_Nucleo" -I"C:/Users/Vlado/workspace/ROBKO_01-STM32/MCU_Firmware/CMSIS/core" -I"C:/Users/Vlado/workspace/ROBKO_01-STM32/MCU_Firmware/CMSIS/device" -O0 -g3 -Wall -fmessage-length=0 -ffunction-sections -c -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 

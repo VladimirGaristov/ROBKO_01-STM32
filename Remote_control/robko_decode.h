e5085f15658ac7115faf853097a580d193287c1b
vl_garistov <vl_garistov@gmail.com>
2020-09-27 21:49:43 +0300

    Added an include guard in robko_decode.h and a response from the server for LAST_CMD

--- Remote_control/robko_decode.h
+++ Remote_control/robko_decode.h
@@ -1,44 +1,49 @@
+#ifndef ROBKO_DEODE_H
+#define ROBKO_DEODE_H
+
 //Commands for remote control
-#define MOV 1
-#define MOVE 2
-#define OFF 3
-#define OPEN_FILE 4
-#define GOTO_POS 5
-#define GET_POS 6
-#define KILL 7
-#define CLEAR 8
-#define FREEZE 9
-#define RESUME 10
-#define SAVE_POS 11
-#define OPTO 12
-#define SET_STEP 13
-#define SET_SPEED 14
-#define GET_STEP 15
-#define GET_SPEED 16
-#define SET_HOME 17
+#define MOV			1
+#define MOVE		2
+#define OFF			3
+#define OPEN_FILE	4
+#define GOTO_POS	5
+#define GET_POS		6
+#define KILL		7
+#define CLEAR		8
+#define FREEZE		9
+#define RESUME		10
+#define SAVE_POS	11
+#define OPTO		12
+#define SET_STEP	13
+#define SET_SPEED	14
+#define GET_STEP	15
+#define GET_SPEED	16
+#define SET_HOME	17
 
-#define GET_POS_REPLY 18
-#define SAVE_POS_REPLY 199
-#define STEP_REPLY 20
-#define SPEED_REPLY 21
-#define ACK 22
-#define ERROR_REPLY 23
-#define LAST_CMD 24
+#define REPEAT		25
 
-#define REPEAT 25
+#define GET_POS_REPLY	18
+#define SAVE_POS_REPLY	19
+#define STEP_REPLY		20
+#define SPEED_REPLY		21
+#define ACK				22
+#define ERROR_REPLY		23
+#define LAST_CMD		24
 
-#define FULL_RAM 101
-#define UNKNOWN_CMD 102
+#define FULL_RAM		101
+#define UNKNOWN_CMD		102
 
-#define FULL_STEP 2
-#define HALF_STEP 1
-#define USE_LOCAL_STEP 0
-#define USE_LOCAL_TIME 0
+#define FULL_STEP		2
+#define HALF_STEP		1
+#define USE_LOCAL_STEP	0
+#define USE_LOCAL_TIME	0
 
 //Socket parameters
-#define SOCK_PORT 55321
-#define SOCK_PROTOCOL SOCK_STREAM	//SOCK_STREAM=TCP
+#define SOCK_PORT		55321
+#define SOCK_PROTOCOL	SOCK_STREAM	//SOCK_STREAM=TCP
 
-#define MAX_FILENAME_SIZE 256
+#define MAX_FILENAME_SIZE	256
 
 int decode_cmd(char *buffer, uint8_t *cmd);
+
+#endif
--- Remote_control/robko_server.c
+++ Remote_control/robko_server.c
@@ -26,7 +26,7 @@
 #define WORD_LENGHT 8
 #define PARITY SP_PARITY_ODD
 #define OUTPUT_FILE "/home/cartogan/Ac6/workspace/ROBKO_01-STM32/Remote_control/position_log.robko"
-#define READ_TIMEOUT 25		//ms
+#define READ_TIMEOUT 25		// ms
 
 #define PARSED_REPEAT 2
 #define PARSED_REPLY 1
@@ -67,26 +67,26 @@ int main(void)
 	int sockfd, clilen;
 	struct sp_port *serial_port;
 
-	//Open serial connection
+	// Open serial connection
 	if (port_configuration(&serial_port))
 	{
 		error("Serial port error\n");
 	}
 
-	//Create a socket for receiving incoming requests
+	// Create a socket for receiving incoming requests
 	sockfd = socket(AF_INET, SOCK_PROTOCOL, 0);
 	if (sockfd < 0)
 	{
 		error("ERROR opening socket\n");
 	}
-	//Set all bits in serv_addr to 0
+	// Set all bits in serv_addr to 0
 	memset((char *) &serv_addr, 0, sizeof(serv_addr));
 	serv_addr.sin_family = AF_INET;
-	//INADDR_ANY is the IP address of the host, running this code
+	// INADDR_ANY is the IP address of the host, running this code
 	serv_addr.sin_addr.s_addr = INADDR_ANY;
 	serv_addr.sin_port = htons(SOCK_PORT);
-	//Bind the socket to the host IP address and port number
-	//sockaddr is a generic socket address, sockaddr_in is for IPv4
+	// Bind the socket to the host IP address and port number
+	// sockaddr is a generic socket address, sockaddr_in is for IPv4
 	if (bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0)
 	{
 		error("ERROR on binding\n");
@@ -97,7 +97,7 @@ int main(void)
 	while (1)
 	{
 		printf("\nAwaiting connection...\n");
-		//Open a socket to communicate with the client
+		// Open a socket to communicate with the client
 		get_client_commands(accept(sockfd, (struct sockaddr *) &cli_addr, (socklen_t *) &clilen), serial_port);
 	}
 
@@ -108,7 +108,7 @@ int main(void)
 int get_client_commands(int client_sockfd, struct sp_port *ser_port)
 {
 	int n, sock_flags;
-	//int bytes_available = 0;
+	// int bytes_available = 0;
 	char sock_buffer[SOCK_BUFFER_SIZE];
 	script_file_t script_file =
 	{
@@ -123,7 +123,7 @@ int get_client_commands(int client_sockfd, struct sp_port *ser_port)
 		printf("ERROR on accept!\n");
 		return -1;
 	}
-	//Set the socket file descriptor to non-blocking mode
+	// Set the socket file descriptor to non-blocking mode
 	sock_flags = fcntl(client_sockfd, F_GETFL, 0);
 	fcntl(client_sockfd, F_SETFL, sock_flags | O_NONBLOCK);
 	puts("Client connected.");
@@ -147,7 +147,7 @@ int get_client_commands(int client_sockfd, struct sp_port *ser_port)
 	while (1)
 	{
 		/*
-		//Moved to secondary thread
+		// Moved to secondary thread
 		switch (parse_reply(ser_port, reply_msg))
 		{
 			case 2:
@@ -164,20 +164,21 @@ int get_client_commands(int client_sockfd, struct sp_port *ser_port)
 		}
 		*/
 
-		//if (ioctl(client_sockfd, FIONREAD, &bytes_available) == 0 && bytes_available > 0)
-		//{
+		// if (ioctl(client_sockfd, FIONREAD, &bytes_available) == 0 && bytes_available > 0)
+		// {
 			memset(sock_buffer, 0, SOCK_BUFFER_SIZE);
 			errno = 0;
 			n = read(client_sockfd, sock_buffer, SOCK_BUFFER_SIZE - 1);
-			//if file, add null byte?
+			// if file, add null byte?
 			
-			//If FIN has been received, close the socket
+			// If FIN has been received, close the socket
 			if (!n)
 			{
-				//add a console message
+				// add a console message
 				shutdown(client_sockfd, SHUT_RDWR);
 				close(client_sockfd);
 				pthread_cancel(reply_thread_handle);
+				pthread_mutex_unlock(&serial_port_mutex);
 				pthread_mutex_destroy(&serial_port_mutex);
 				return 0;
 			}
@@ -194,6 +195,7 @@ int get_client_commands(int client_sockfd, struct sp_port *ser_port)
 					shutdown(client_sockfd, SHUT_RDWR);
 					close(client_sockfd);
 					pthread_cancel(reply_thread_handle);
+					pthread_mutex_unlock(&serial_port_mutex);
 					pthread_mutex_destroy(&serial_port_mutex);
 					return -2;
 				}
@@ -218,7 +220,7 @@ int get_client_commands(int client_sockfd, struct sp_port *ser_port)
 				transmit_string(ser_port, sock_buffer);
 				pthread_mutex_unlock(&serial_port_mutex);
 			}
-		//}
+		// }
 	}
 }
 
@@ -238,11 +240,11 @@ void delay(int msec)
 	}
 }
 
-//Initialize the serial port
+// Initialize the serial port
 int port_configuration(struct sp_port **ser_port)
 {
 	int check;
-	//Decode port name
+	// Decode port name
 	sp_get_port_by_name(SERIAL_PORT, ser_port);
 	if (ser_port == NULL)
 	{
@@ -292,7 +294,7 @@ int execute_file(script_file_t *file, struct sp_port *ser_port)
 		return -1;
 	}
 	printf("Executing %s\n", file->filename);
-	//Reads one line at a time
+	// Reads one line at a time
 	while (fgets(file_buffer, FILE_BUFFER_SIZE - 1, fd))
 	{
 		decode_status = decode_cmd(file_buffer, cmd);
@@ -308,7 +310,7 @@ int execute_file(script_file_t *file, struct sp_port *ser_port)
 		{
 			printf("Invalid command - \"%s\"\n", file_buffer);
 		}
-		//TODO parse reply and check for error or ACK
+		// TODO parse reply and check for error or ACK
 	}
 	printf("Done\n");
 	fclose(fd);
@@ -319,9 +321,9 @@ int parse_reply(struct sp_port *ser_port, char *msg)
 {
 	uint8_t reply[13] = {0}, read_status = 0;
 	uint16_t speed = 0;
-	//pthread_mutex_lock(&serial_port_mutex);
+	pthread_mutex_lock(&serial_port_mutex);
 	read_status = sp_blocking_read(ser_port, reply, 1, READ_TIMEOUT);
-	//pthread_mutex_unlock(&serial_port_mutex);
+	pthread_mutex_unlock(&serial_port_mutex);
 	if (!read_status)
 	{
 		return 0;
@@ -388,7 +390,7 @@ int parse_reply(struct sp_port *ser_port, char *msg)
 					return -1;
 				}
 				int16_t *reply_pos = (int16_t *) (reply + 1);
-				//No hate pls
+				// No hate pls
 				sprintf(
 					msg,
 					"Current motor positions:\n"
@@ -410,6 +412,7 @@ int parse_reply(struct sp_port *ser_port, char *msg)
 				}
 				return PARSED_REPLY;
 			case LAST_CMD:
+				sprintf(msg, "Last command has finished executing.\n");
 				return PARSED_REPEAT;
 			case ERROR_REPLY:
 				pthread_mutex_lock(&serial_port_mutex);
@@ -467,17 +470,23 @@ int send_reply(int client_sockfd, char *reply_msg)
 	return 0;
 }
 
-// Secondary thread handles replies from the MCU to the client
-// and repeating files
+// Secondary thread handles replies from the MCU to the client and repeating files
 void *reply_thread(void *args)
 {
 	char reply_msg[REPLY_MSG_SIZE];
 	thread_args_t *arguments = (thread_args_t *) args;
+
 	while (1)
 	{
 		switch (parse_reply(arguments->ser_port, reply_msg))
 		{
 			case PARSED_REPEAT:
+				#ifdef EN_DEBUG
+				fputs(reply_msg, stdout);
+				fflush(stdout);
+				#endif
+				send_reply(arguments->sockfd, reply_msg);
+
 				if (arguments->script_file->repeat)
 				{
 					pthread_mutex_lock(&serial_port_mutex);
@@ -485,6 +494,7 @@ void *reply_thread(void *args)
 					pthread_mutex_unlock(&serial_port_mutex);					
 				}
 				break;
+
 			case PARSED_REPLY:
 				#ifdef EN_DEBUG
 				fputs(reply_msg, stdout);
@@ -492,9 +502,10 @@ void *reply_thread(void *args)
 				#endif
 				send_reply(arguments->sockfd, reply_msg);
 				break;
+
 			default:;
 		}
-
 	}
+
 	return NULL;
 }
